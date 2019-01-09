f6cf554a0fc25a1f8e0aa119f68c7dca5576447a
Felglitzzz <felix.amande@andela.com>
2019-01-09 23:14:48 +0100

    initial commit

---
+++ src/app/components/user/user.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  // styleUrls: ['./user.component.css'],
+  // styles: [
+  //   `
+  //   h2 {
+  //     color: blue
+  //   }
+  //   `
+  // ]
+  // template: '<h2>John  Doe</h2>'
+})
+
+export class UserComponent {
+
+} 
